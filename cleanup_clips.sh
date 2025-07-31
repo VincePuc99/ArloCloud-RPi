481efee542506133a2f7f8252afbfa09f0c9fc73
Vincenzo Puca <32323498+VincePuc99@users.noreply.github.com>
2025-07-31 14:58:39 +0200

    Delete cleanup_clips.sh

--- cleanup_clips.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-CLIP_STORE="$SCRIPT_DIR/ArloExposed"
-RETENTION_DURATION=14
-
-find "$CLIP_STORE" -maxdepth 5 -type f -mtime +"$RETENTION_DURATION" -print | xargs -r rm
-find "$CLIP_STORE"/arlo/metadata/ -type d -empty -delete
