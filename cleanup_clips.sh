4263b78e78998a9cc04b70352f7a6cd967dbe33d
Vincenzo Puca <32323498+VincePuc99@users.noreply.github.com>
2025-07-31 13:08:40 +0200

    Update cleanup_clips.sh

--- cleanup_clips.sh
+++ cleanup_clips.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-CLIP_STORE="$(dirname "$0")/ArloExposed"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CLIP_STORE="$SCRIPT_DIR/ArloExposed"
 RETENTION_DURATION=14
 
 find "$CLIP_STORE" -maxdepth 5 -type f -mtime +"$RETENTION_DURATION" -print | xargs -r rm
