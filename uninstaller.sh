259a1b5eeb2b1ed33a8a732f4d4500ef37920481
Vincenzo Puca <32323498+VincePuc99@users.noreply.github.com>
2026-05-19 17:18:57 +0200

    added logging feature

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+LOG_FILE="$SCRIPT_DIR/arlo_usb_uninstaller.log"
 
 echo "#############################################"
 echo "#### ArloCloud-RPi All-in-one uninstaller ###"
 echo "#############################################"
 echo
 
-echo "Checking if 'dwc2' is present in /etc/modules and /boot/config.txt..."
+############################################# LogFile
+
+if [ -f "$LOG_FILE" ]; then
+    > "$LOG_FILE"
+else
+    touch "$LOG_FILE"
+fi
+
+log() {
+    echo "$1" | tee -a "$LOG_FILE"
+}
+
+#############################################
+
+log "Checking if 'dwc2' is present in /etc/modules and /boot/config.txt..."
 
 if grep -q "dwc2" /etc/modules; then
-    echo "'dwc2' found in /etc/modules. Proceeding to remove..."
+    log "'dwc2' found in /etc/modules. Proceeding to remove..."
     if sudo sed -i '/dwc2/d' /etc/modules; then
-        echo "'dwc2' removed from /etc/modules successfully."
+        log "'dwc2' removed from /etc/modules successfully."
     else
-        echo "Failed to remove 'dwc2' from /etc/modules."
+        log "Failed to remove 'dwc2' from /etc/modules."
         exit 1
     fi
 else
-    echo "'dwc2' not found in /etc/modules. Skipping removal."
+    log "'dwc2' not found in /etc/modules. Skipping removal."
 fi
 
 echo
 
 if grep -q "dtoverlay=dwc2" /boot/config.txt; then
-    echo "'dtoverlay=dwc2' found in /boot/config.txt. Proceeding to remove..."
+    log "'dtoverlay=dwc2' found in /boot/config.txt. Proceeding to remove..."
     if sudo sed -i '/dtoverlay=dwc2/d' /boot/config.txt; then
-        echo "'dtoverlay=dwc2' removed from /boot/config.txt successfully."
+        log "'dtoverlay=dwc2' removed from /boot/config.txt successfully."
     else
-        echo "Failed to remove 'dtoverlay=dwc2' from /boot/config.txt."
+        log "Failed to remove 'dtoverlay=dwc2' from /boot/config.txt."
         exit 1
     fi
 else
-    echo "'dtoverlay=dwc2' not found in /boot/config.txt. Skipping removal."
+    log "'dtoverlay=dwc2' not found in /boot/config.txt. Skipping removal."
 fi
 
 echo
@@ -43,16 +58,16 @@ ARLO_IMG_FILE="/arlo.bin"
 ARLO_IMG_MOUNT_POINT="$SCRIPT_DIR/arlo" 
 ARLO_EXPOSED_MOUNT_POINT="$SCRIPT_DIR/ArloExposed" 
 
-echo "Removing ARLO image file and mount folders..."
+log "Removing ARLO image file and mount folders..."
 
 if mountpoint -q "$ARLO_IMG_MOUNT_POINT"; then
-    sudo umount "$ARLO_IMG_MOUNT_POINT" || { echo "Failed to unmount $ARLO_IMG_MOUNT_POINT"; exit 1; }
+    sudo umount "$ARLO_IMG_MOUNT_POINT" || { log "Failed to unmount $ARLO_IMG_MOUNT_POINT"; exit 1; }
 fi
 
 if sudo rm -rf "$ARLO_IMG_MOUNT_POINT" && rm -f "$ARLO_IMG_FILE" && sudo rm -rf "$ARLO_EXPOSED_MOUNT_POINT"; then
-    echo "ARLO image and folders removed successfully."
+    log "ARLO image and folders removed successfully."
 else
-    echo "Failed to remove ARLO image or mount folders."
+    log "Failed to remove ARLO image or mount folders."
     exit 1
 fi
 
@@ -60,13 +75,13 @@ echo
 echo "#############################################"
 echo
 
-echo "Checking if cron jobs exist..."
+log "Checking if cron jobs exist..."
 
 if crontab -l | grep -vE "@reboot sudo sh $SCRIPT_DIR/enable_mass_storage.sh [0-9]+" | crontab - && \
    crontab -l | grep -vE "\\*/1 \\* \\* \\* \\* sudo /bin/bash $SCRIPT_DIR/sync_clips.sh" | crontab - ; then
-   echo "All cron jobs found and removed."
+   log "All cron jobs found and removed."
 else
-    echo "One or more cron jobs not found. Skipping removal."
+    log "One or more cron jobs not found. Skipping removal."
     exit 1
 fi
 
@@ -74,14 +89,14 @@ echo
 echo "#############################################"
 echo
 
-echo "Removing all ArloCloud-RPi related files (including this uninstaller)..."
+log "Removing all ArloCloud-RPi related files (including this uninstaller)..."
 
 if [ $(basename "$SCRIPT_DIR") == "ArloCloud-RPi" ]; then
     
     if rm -rf $SCRIPT_DIR; then
-        echo "All config files removed successfully."
+        log "All config files removed successfully."
     else
-        echo "Failed to remove config files."
+        log "Failed to remove config files."
         exit 1
     fi
 else
@@ -93,6 +108,6 @@ echo
 echo "#############################################"
 echo
 
-echo "All tasks completed successfully. Rebooting..."
+log "All tasks completed successfully. Rebooting..."
 
 sudo reboot
